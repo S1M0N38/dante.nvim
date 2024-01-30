00c1a71b5f60d82b56010b1861185123bb03d75e
S1M0N38 <bertolottosimone@gmail.com>
2024-01-30 22:48:10 +0100

    feat(config): extend preset with global config

--- lua/dante/config.lua
+++ lua/dante/config.lua
@@ -1,15 +1,29 @@
 local M = {}
 
 local defaults = {
+
 	presets = {
 		default = {
-			model = "gpt-3.5-turbo",
-			prompt = "You are an helpful assistant.",
-			temperature = 1,
-			stream = false,
+			-- if not specified, will use global config
+			-- model = "gpt-3.5-turbo",
+			-- prompt = "You are an helpful assistant.",
+			-- temperature = 1,
+			-- stream = false,
+			-- base_url = "https://api.openai.com/v1",
+			-- diffopt = { ... },
 		},
+		-- another_preset = {
+		--  -- you can define as many presets as you want
+		--  -- defining config here will override global config for this preset
+		-- },
 	},
-	openai_api_key = "OPENAI_API_KEY",
+
+	-- Globals config will be used as default for all presets
+	model = "gpt-3.5-turbo",
+	prompt = "You are an helpful assistant.",
+	temperature = 1,
+	stream = false,
+	env_var = "OPENAI_API_KEY",
 	base_url = "https://api.openai.com/v1",
 	diffopt = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
 }
@@ -18,6 +32,13 @@ M.options = {}
 
 function M.setup(options)
 	M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
+	for _, preset in pairs(M.options.presets) do
+		for opt, value in pairs(M.options) do
+			if opt ~= "presets" then
+				preset[opt] = preset[opt] or value
+			end
+		end
+	end
 end
 
 return M
