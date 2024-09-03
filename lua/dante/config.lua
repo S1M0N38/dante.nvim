9a9e7415012da0d5d86f1a325df280b58bf0de96
S1M0N38 <bertolottosimone@gmail.com>
2024-09-03 09:05:40 +0200

    feat: update default config with most recent model

--- lua/dante/config.lua
+++ lua/dante/config.lua
@@ -2,43 +2,43 @@ local config = {}
 
 config.defaults = {
 
-	-- notify with extra information
-	verbose = false,
+  -- notify with extra information
+  verbose = false,
 
-	-- list of presets
-	presets = {
-		["default"] = {
-			client = {
-				base_url = "https://api.openai.com/v1",
-				api_key = nil,
-			},
-			request = {
-				model = "gpt-3.5-turbo",
-				temperature = 1.0,
-				stream = false,
-				messages = {
-					{
-						role = "system",
-						content = [[
+  -- list of presets
+  presets = {
+    ["default"] = {
+      client = {
+        base_url = "https://api.openai.com/v1",
+        api_key = nil,
+      },
+      request = {
+        model = "gpt-4o-mini",
+        temperature = 0.0001, -- some provider complain about integer values
+        stream = false,
+        messages = {
+          {
+            role = "system",
+            content = [[
 You are an assistant responsible for correcting errors in text.
 Refine the spelling and grammar while closely adhering to the original version.
 
 - If the text is formatted in a specific syntax (e.g. LaTeX, Markdown, Vimdoc, ...), abide by that syntax.
 - Use the same language and terminology appropriate for the context.
 - Return only the enhanced text without commentary.
-- Maintain the integrity of the original text's line breaks and spacing.
+- Maintain the integrity of the original text's line breaks and spacing (i.e. follow the original text's `\n`)
 
 Do NOT return the generated text enclosed in triple ticks (```).
 ]],
-					},
-					{
-						role = "user",
-						content = "{{'<,'>}}",
-					},
-				},
-			},
-		},
-	},
+          },
+          {
+            role = "user",
+            content = "{{'<,'>}}",
+          },
+        },
+      },
+    },
+  },
 }
 
 ---@class ClientOptions
@@ -58,7 +58,7 @@ config.options = {}
 ---It must be called before using other ai.nvim functions.
 ---@param opts Options: config table
 config.setup = function(opts)
-	config.options = vim.tbl_deep_extend("force", {}, config.defaults, opts or {})
+  config.options = vim.tbl_deep_extend("force", {}, config.defaults, opts or {})
 end
 
 return config
