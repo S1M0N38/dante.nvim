196f398c740d4a5543b1805bca91a62461da92e0
S1M0N38 <bertolottosimone@gmail.com>
2024-09-04 13:44:16 +0200

    feat: Dante command autocomplete with preset

--- plugin/dante.lua
+++ plugin/dante.lua
@@ -1,10 +1,49 @@
-vim.api.nvim_create_user_command("Dante", function(args)
-  if args.range ~= 2 then
-    vim.notify("Please select a range of text.", vim.log.levels.WARN)
+local function preset_keys()
+  local presets = require("dante.config").options.presets
+  local keys = {}
+  for key, _ in pairs(presets) do
+    table.insert(keys, key)
+  end
+  return keys
+end
+
+local function is_available_preset(preset)
+  local presets = require("dante.config").options.presets
+  return presets[preset] ~= nil
+end
+
+local function dante_cmd(opts)
+  -- Force the user to select a range of text
+  if opts.range ~= 2 then
+    vim.notify("Dante: please select a range of text.", vim.log.levels.WARN)
     return
   end
-  if args.args == "" then
-    args.args = "default"
+
+  -- Check if the user selected a valid preset
+  local selected_preset = opts.args or "default"
+  if selected_preset == "" then
+    selected_preset = "default"
   end
-  require("dante").main(args.args, args.line1, args.line2)
-end, { range = true, nargs = "?" })
+  if not is_available_preset(selected_preset) then
+    vim.notify("Dante: " .. selected_preset .. " is an invalid preset", vim.log.levels.WARN)
+    vim.notify("Dante: available presets are: " .. vim.inspect(preset_keys()), vim.log.levels.WARN)
+    return
+  end
+
+  vim.notify("Dante: running with preset " .. selected_preset)
+  require("dante").main(selected_preset, opts.line1, opts.line2)
+end
+
+vim.api.nvim_create_user_command("Dante", dante_cmd, {
+  nargs = "?",
+  range = true,
+  desc = "Dante command with preset completions",
+  complete = function(arg_lead, _, _)
+    return vim
+      .iter(preset_keys())
+      :filter(function(preset)
+        return preset:find(arg_lead) ~= nil
+      end)
+      :totable()
+  end,
+})
